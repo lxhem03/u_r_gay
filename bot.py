fa3df8fd1b2e2e42c0116aa65038b3df6df58b55
Levhcwm21. <alexags1983@gmail.com>
2025-07-03 20:58:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 nectar = Client("Telegram-Guy", api_id=API_ID, api_hash=API_HASH, plugins={"root": "plugins"},bot_token=BT)
 
-print("Nectar is started....")
 
-if name = "__main__"
+if name == "__main__"
+    print("Nectar is started....")
     nectar.run()
