d0fba46a9c416cf0c646d7ed30bb113c594943aa
Levhcwm21. <alexags1983@gmail.com>
2025-07-03 11:16:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+
+BT = environ.get("BT", "")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+DB = environ.get("DB", "")
+DB_N = environ.get ("DB_N", "")
