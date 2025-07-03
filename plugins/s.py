b1a41d585038b7fa1774d58d4b3695b1346e9d74
Levhcwm21. <alexags1983@gmail.com>
2025-07-03 20:47:41 +0530

    Create s.py

---
+++ plugins/s.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+import asyncio
+
+
+@nectar.on_message(filters.command("start"))
+async def s_command(client, message):
+  s_m = (
+    "Hello there! I'm a advance bot create by @The_TGguy"
+  )
+  await message.reply_text(s_m)
+
+
+@nectar.on_message(filters.command("help"))
+async def help_command(client, message):
+    help_text = (
+        "Available commands:\n"
+        "/start - Start the bot\n"
+        "/help - Show this help message\n"
+        "Send any text to echo it back!"
+    )
+    await message.reply_text(help_text)
