3a9225e0d78c36eedf0cd4bbe1fb879e7b1466ad
Levhcwm21. <alexags1983@gmail.com>
2025-07-03 15:10:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import logging
 import logging.config
 from pyrogram import Client 
+from config import BT, API_ID, API_HASH
 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+Nectar = Client("Telegram-Guy",api_id=API_ID,api_hash=API_HASH,bot_token=BT)
