71a8f1a43a1945f96b481a3a20a4888df5a91114
Levhcwm21. <alexags1983@gmail.com>
2025-07-06 14:13:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 BT = os.environ.get("BT", "7721902522:AAFnaEw9JuYmmfqPybFkgX60mGqO-fk9bJE")
 API_ID = int(os.environ.get("API_ID", "27394279"))
 API_HASH = os.environ.get("API_HASH", "90a9aa4c31afa3750da5fd686c410851")
-DB = os.environ.get("DB", "")
-DB_N = os.environ.get ("DB_N", "")
+DB = os.environ.get("DB", "mongodb+srv://python21java:8ZFGYMKJCqAPwsiO@filestore.f876hjv.mongodb.net/?retryWrites=true&w=majority&appName=Filestore")
+DB_N = os.environ.get("DB_N", "try")
+LG_C = int(os.environ.get("LG_C", "-1002288135729"))
