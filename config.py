52a6dca6dffd9773c172bf3af54460e1df907e90
Levhcwm21. <alexags1983@gmail.com>
2025-07-06 12:50:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os, environ
+import os
 
-BT = environ.get("BT", "7721902522:AAFnaEw9JuYmmfqPybFkgX60mGqO-fk9bJE")
-API_ID = int(environ.get("API_ID", "27394279"))
-API_HASH = environ.get("API_HASH", "90a9aa4c31afa3750da5fd686c410851")
-DB = environ.get("DB", "")
-DB_N = environ.get ("DB_N", "")
+BT = os.environ.get("BT", "7721902522:AAFnaEw9JuYmmfqPybFkgX60mGqO-fk9bJE")
+API_ID = int(os.environ.get("API_ID", "27394279"))
+API_HASH = os.environ.get("API_HASH", "90a9aa4c31afa3750da5fd686c410851")
+DB = os.environ.get("DB", "")
+DB_N = os.environ.get ("DB_N", "")
