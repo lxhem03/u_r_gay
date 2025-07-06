3eeb451c88f3a6f165923b79a40499dd1fbd9485
Levhcwm21. <alexags1983@gmail.com>
2025-07-06 14:15:48 +0530

    Update s.py

--- plugins/s.py
+++ plugins/s.py
@@ -1,10 +1,14 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
-
+from db import tg 
+from config import LG_C
 
 @Client.on_message(filters.command("start"))
 async def s_command(client, message):
+  if await tg.get_user(message.from_user.id) is None:
+        await tg.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LG_C, text="#New_User\n\nUser: {}ID: {}".format(message.from_user.mention, message.from_user.id))
   s_m = (
     "Hello there! I'm a advance bot create by @The_TGguy"
   )
